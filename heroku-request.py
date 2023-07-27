8f97ab3e066486675c15903a8e0bbc8c4bc045c4
huynguyenanh24 <huynguyenanh24@gmail.com>
2023-07-27 14:05:09 +0700

    update Procfile

---
+++ heroku-request.py
@@ -0,0 +1,36 @@
+"""
+Test the live prediction endpoint on Heroku
+"""
+import requests
+import logging
+
+logging.basicConfig(filename='./test_heroku.log', level=logging.INFO, format="%(asctime)-15s %(message)s")
+
+
+features = {
+    "age": 36,
+    "workclass": "Private",
+    "fnlgt": 302146,
+    "education": "HS-grad",
+    "education_num": 9,
+    "marital_status": "Divorced",
+    "occupation": "Craft-repair",
+    "relationship": "Husband",
+    "race": "White",
+    "sex": "Male",
+    "capital_gain": 2000,
+    "capital_loss": 0,
+    "hours_per_week": 45,
+    "native_country": "United-States"
+}
+
+
+app_url = "https://ml-fastapi-96af12f40f4e.herokuapp.com/"
+
+r = requests.post(app_url, json=features)
+print(r.status_code)
+# assert r.status_code == 200
+
+# logging.info("Testing Heroku app")
+# logging.info(f"Status code: {r.status_code}")
+# logging.info(f"Response body: {r.json()}")
